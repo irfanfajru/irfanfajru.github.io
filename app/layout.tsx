f54c9181953f8bc6eac8b42ee4c418df86bb849f
irfanfajru <irfanfajru025@gmail.com>
2023-09-15 15:17:16 +0700

    fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,13 @@ const font = Poppins({ subsets: ['latin'],weight:['300'] })
 
 export const metadata: Metadata = {
   title: 'Muhammad Irfan Fajru Ramadhan',
-  description: 'Muhammad Irfan Fajru Ramadhan',
+  description: "Build server-side logic and database architecture that powers web and mobile applications, ensuring they function smoothly and securely.Experienced with programming languages like PHP, JavaScript, Python, Golang, and Java. Collaborate closely with front-end developers to create cohesive and efficient software solutions.",
+  icons: {
+    icon: ['/favicon.ico?v=4'],
+    apple: ['/apple-touch-icon.png?v=4'],
+    shortcut: ['/apple-touch-icon.png'],
+  },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
